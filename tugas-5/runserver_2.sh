b75ca1462f4a2408dd9fef38aa75637e7a61e80e
Vito Febrian Ananta <vitoananta3@gmail.com>
2024-06-27 19:24:51 +0700

    init tugas-5: add all files

---
+++ tugas-5/runserver_2.sh
@@ -0,0 +1,6 @@
+#jalankan 4 thread_server
+
+python3 server_process_pool_http.py 10002 &
+python3 server_process_pool_http.py 10003 &
+python3 server_process_pool_http.py 10004 &
+python3 server_process_pool_http.py 10005 &
